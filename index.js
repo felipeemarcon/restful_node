9cd9ec26794bdd5c14a37fb06b4b0fefbd62aab6
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-28 21:57:37 -0300

    NodeJS, server create, urls, package.json, nodemon and express

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type.', 'text/html');
+  res.end('<h1>Olá</h1>');
+
+});
+
+app.get('/users', (req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type.', 'text/html');
+  res.json({
+    users:[{
+      name: 'Felipe Marcon',
+      email: 'oi@felipemarcon.com.br',
+      id: 1
+    }]
+  });
+
+});
+
+app.listen(3000, '127.0.0.1', () => {
+  
+  console.log('Servidor rodando!');
+  
+});
