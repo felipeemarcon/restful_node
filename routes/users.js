c7d7d76038cae8a91b42a1986755157541f39a83
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-29 22:11:32 -0300

    Data persistence, listing users, refacatoring, get user data, edit user, delete user, data validate

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,89 @@ let db = new NeDB({
   autoload:true
 });
 
-module.exports = (app) => {
-  app.get('/users', (req, res) => {
+module.exports = app => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type.', 'text/html');
-    res.json({
-      users: [{
-        name: 'Felipe Marcon',
-        email: 'oi@felipemarcon.com.br',
-        id: 1
-      }]
+  let route = app.route('/users');
+
+  route.get((req, res) => {
+
+    db.find({}).sort({name:1}).exec((err, users) => {
+
+      if (err) {
+
+        app.utils.error.send(err, req, res);
+
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users
+        });
+      }
+      
     });
 
   });
 
-  app.post('/users', (req, res) => {
+  route.post((req, res) => {
 
+    if (!app.utils.validator.user(app, req, res)) return false;
+    
     db.insert(req.body, (err, user) => {
+
       if(err) {
-
-        console.log(`error: ${err}`);
-        res.status(400).json({
-          error: err
-        });
-
+        app.utils.error.send(err, req, res);
       } else {
-
         res.status(200).json(user);
-
       }
     });
 
   });
+
+   let routeId  = app.route('/users/:id')
+
+   routeId.get((req, res) => {
+
+    db.findOne({_id:req.params.id}).exec((err, user) => {
+
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+
+    });
+
+   }); 
+
+   routeId.put((req, res) => {
+
+    if (!app.utils.validator.user(app, req, res)) return false;
+
+    db.update({_id:req.params.id}, req.body, err => {
+
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(Object.assign(req.body, req.params));
+      }
+
+    });
+
+   }); 
+
+   routeId.delete((req, res) => {
+
+    db.remove({_id:req.params.id}, {}, err => {
+
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(req.params);
+      }
+
+    });
+
+   }); 
+
 };
